05ec8d3c4102948052b747ece4f7307d5f17b8a9
MrRavin1 <29rabinkarki@gmail.com>
2026-07-22 15:48:02 +0545

    debug: enable APP_DEBUG and artisan about to diagnose 500 error

--- docker/start.sh
+++ docker/start.sh
@@ -11,5 +11,8 @@ php artisan view:cache
 # Run migrations
 php artisan migrate --force
 
+# Test the app boots correctly and show any errors
+php artisan about --no-ansi 2>&1 || true
+
 # Start supervisor (runs nginx + php-fpm + queue)
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
