f926f92417f85807af8b4cd6437d4aa8e084cebc
MrRavin1 <29rabinkarki@gmail.com>
2026-07-22 16:50:46 +0545

    fix: remove Cloudflare plugin (CSP eval error), force HTTPS in production

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
-    plugins: [laravel({
-        input: 'resources/js/app.tsx',
-        refresh: true,
-    }), react(), cloudflare()],
+    plugins: [
+        laravel({
+            input: 'resources/js/app.tsx',
+            refresh: true,
+        }),
+        react(),
+    ],
 });
